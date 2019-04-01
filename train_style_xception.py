f02c2f4b5c5f3c6d12d5d9eb9c5540f977c9b053
jreganpb <jregan@photobutler.com>
2019-04-01 16:10:38 -0400

    Initial style modeling

---
+++ train_style_xception.py
@@ -0,0 +1,86 @@
+import os
+
+from keras.models import Model
+from keras.layers import Dense, Dropout
+import keras
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.optimizers import Adam
+
+from utilities.keras_style_loader import train_labels, train_files, test_labels, test_files, image_generator
+
+'''
+Below is a modification to the TensorBoard callback to perform
+batchwise writing to the tensorboard, instead of only at the end
+of the batch.
+'''
+class TensorBoardBatch(TensorBoard):
+    def __init__(self, *args, **kwargs):
+        super(TensorBoardBatch, self).__init__(*args)
+
+        # conditionally import tensorflow iff TensorBoardBatch is created
+        self.tf = __import__('tensorflow')
+
+    def on_batch_end(self, batch, logs=None):
+        logs = logs or {}
+
+        for name, value in logs.items():
+            if name in ['batch', 'size']:
+                continue
+            summary = self.tf.Summary()
+            summary_value = summary.value.add()
+            summary_value.simple_value = value.item()
+            summary_value.tag = name
+            self.writer.add_summary(summary, batch)
+
+        self.writer.flush()
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+
+        for name, value in logs.items():
+            if name in ['batch', 'size']:
+                continue
+            summary = self.tf.Summary()
+            summary_value = summary.value.add()
+            summary_value.simple_value = value.item()
+            summary_value.tag = name
+            self.writer.add_summary(summary, epoch * self.batch_size)
+
+        self.writer.flush()
+
+## Dataset is much smaller here, so we can load dataset in its entirety
+
+image_size = 224
+
+base_model = keras.applications.Xception(input_shape=(image_size, image_size, 3), include_top=False, pooling='avg')
+for layer in base_model.layers:
+    layer.trainable = False
+
+x = Dropout(0.75)(base_model.output)
+x = Dense(14, activation='softmax')(x)
+
+model = Model(base_model.input, x)
+#model.summary()
+optimizer = Adam(lr=1e-3)
+model.compile(optimizer, loss='binary_crossentropy')
+
+# load weights from trained model if it exists
+if os.path.exists('style_weights/xception_weights.h5'):
+    model.load_weights('style_weights/xception_weights.h5')
+
+# load pre-trained NIMA(Inception ResNet V2) classifier weights
+# if os.path.exists('weights/inception_resnet_pretrained_weights.h5'):
+#     model.load_weights('weights/inception_resnet_pretrained_weights.h5', by_name=True)
+
+checkpoint = ModelCheckpoint('style_weights/xception_weights.h5', monitor='val_loss', verbose=1, save_weights_only=True, save_best_only=True,
+                             mode='min')
+tensorboard = TensorBoardBatch()
+callbacks = [checkpoint, tensorboard]
+
+batchsize = 256
+epochs = 20
+
+model.fit_generator(image_generator(files=train_files,scores=train_labels,batch_size=batchsize),
+                    steps_per_epoch=len(train_files) // batchsize, epochs=epochs,
+                    validation_data=image_generator(files=test_files,scores=test_labels,batch_size=batchsize),
+                    validation_steps=len(test_files) // batchsize,callbacks=callbacks)
---
+++ utilities/keras_style_loader.py
@@ -0,0 +1,133 @@
+import numpy as np
+import os
+import tensorflow as tf
+import cv2
+from keras.preprocessing.image import ImageDataGenerator
+import pandas as pd
+import gc
+from sklearn.preprocessing import OneHotEncoder
+
+# path to the images and the text file which holds the scores and ids
+base_images_path = r'/mnt/ds3rdparty/AVA/data/images/'
+base_data_path = r'/mnt/ds3rdparty/AVA/style_image_lists/'
+base_disk_path = r'/home/ubuntu/AVA/images/'
+ava_dataset_path = r'/mnt/ds3rdparty/AVA/AVA.txt'
+
+IMAGE_SIZE = 224 # Keras accepts None for height and width fields.
+
+def get_available_files_s3(pathname,bucket='ds3rdparty',callsystem=False):
+    if callsystem:
+        os.system('rm /home/ubuntu/data.txt')
+        q = 'sudo aws s3 ls s3://' + bucket + '/' + pathname + '/ > /home/ubuntu/data.txt'
+        os.system(q)
+    with open('/home/ubuntu/data.txt', 'r') as f:
+        dat = f.readlines()
+    dat = sorted(dat)
+    iidnums = {}
+    files = []
+    for i in dat:
+        tmp = i.rstrip().split()
+        iid = int(tmp[3].split('.')[0])
+        iidnums[iid] = 1
+        files.append(base_images_path + tmp[3])
+    return iidnums, sorted(files)
+
+def get_available_files_disk(pathname='/home/ubuntu/AVA/images/'):
+    l = os.listdir(pathname)
+    iidnums = {}
+    files = []
+    for i in l:
+        iid = int(i[0].split('.')[0])
+        iidnums[iid] = 1
+        files.append(pathname + i)
+    return iidnums, files
+
+def pad_image(image):
+    height, width, dim = image.shape
+    diff = int(float(abs(width-height)) / 2.)
+    top = 0; bottom = 0; left = 0; right = 0
+    if height >= width: # Pad wide
+        left = diff; right = diff
+    elif width >= height: # Pad height
+        top = diff; bottom = diff
+    image = cv2.copyMakeBorder(image,top,bottom,left,right,cv2.BORDER_CONSTANT,value=[0,0,0])
+    return image
+
+def parse_data_without_augmentation(filename):
+    '''
+    Loads the image file without any augmentation. Used for validation set.
+
+    Args:
+        filename: the filename from the record
+        scores: the scores from the record
+
+    Returns:
+        an image referred to by the filename and its scores
+    '''
+    image = cv2.imread(filename)
+    image = pad_image(image)
+    image = cv2.resize(image,(IMAGE_SIZE,IMAGE_SIZE))
+    image = image / 255.
+    return image
+
+def image_generator(files,scores,batch_size=64):
+    while True:
+        paths = np.random.choice(a=files,size=batch_size)
+        batch_input = []; batch_output = []; batch_weight = []
+        for filename in paths:
+            #img = cv2.imread(filename)
+            f2 = filename.split('/')
+            fname = f2[len(f2) - 1]
+            fsplit = fname.split('.')
+            yvar = scores[int(fsplit[0])]
+            try:
+                inp = parse_data_without_augmentation(filename)
+            except Exception:
+                continue
+            batch_input.append(inp); batch_output.append(yvar)
+        batch_x = np.array(batch_input)
+        batch_y = np.array(batch_output)
+        yield(batch_x,batch_y)
+
+with open(base_data_path + 'train.jpgl','r') as f:
+    style_images = f.readlines()
+    for d in range(len(style_images)):
+        style_images[d] = int(style_images[d].rstrip())
+
+with open(base_data_path + 'train.lab','r') as f:
+    style_labels = f.readlines()
+    for d in range(len(style_labels)):
+        style_labels[d] = int(style_labels[d].rstrip())
+
+train_files = []; train_labels = []
+all_files = os.listdir(base_disk_path)
+
+for ix, i in enumerate(style_images):
+    if str(i) + '.jpg' not in all_files:
+        print('%d not found in directory, continuing...' % (i))
+        continue
+    fname = base_disk_path + '/' + str(i) + '.jpg'
+    train_files.append(fname); train_labels.append(style_labels[ix])
+
+enc = OneHotEncoder()
+train_files = np.array(train_files); train_labels = enc.fit_transform(np.array(train_labels).reshape(len(train_labels),1)).toarray()
+
+with open(base_data_path + 'test.jpgl','r') as f:
+    tmp_images = f.readlines()
+    for d in range(len(tmp_images)):
+        tmp_images[d] = int(tmp_images[d].rstrip())
+
+with open(base_data_path + 'test.multilab','r') as f:
+    tmp = f.readlines()
+    for d in range(len(tmp)):
+        tmp[d] = [int(x) for x in tmp[d].rstrip().split(' ')]
+
+test_files = []; test_labels = []
+for ix, i in enumerate(tmp_images):
+    if str(i) + '.jpg' not in all_files:
+        print('%d not found in directory, continuing...' % (i))
+        continue
+    fname = base_disk_path + '/' + str(i) + '.jpg'
+    test_files.append(fname); test_labels.append(tmp[ix])
+
+test_files = np.array(test_files); test_labels = np.array(test_labels)
